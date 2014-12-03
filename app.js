4c5bbd3a8c8890653f66ca406667acc9439a9f7e
courtney <courtney@localhost.localdomain>
2014-12-03 10:03:58 -0700

    ajax requestsss

--- app.js
+++ app.js
@@ -4,15 +4,3 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-var router = express.Router();
-//app.use('/assets', express.static(__dirname+'/assets'));
-//app.use('view engine', 'ejs');
-
-router.get('/', function(req,res){
-	res.render('layout', {content:'./views/login.ejs'});				
-});
-router.post('/home', function(req, res){
-				console.log('GOT HERE!');
-				console.log(req);
-	res.sendFile(__dirname + '/home.html');				
-});
