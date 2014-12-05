e174fc8f62b525b7a854fb2a29c2bb59da1668dc
courtney <courtney@localhost.localdomain>
2014-12-05 02:34:14 -0700

    progress

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+ var express = require('express');
+ var app = express();
+app.use(express.static(__dirname + '/assets'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+// var bodyParser = require('body-parser');
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:true}));
+
+
+var db = require('diskdb');
 
